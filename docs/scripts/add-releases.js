1a404d06fb77bae278481228e3a488c0f41197b0
QualiTTV <quali_plays@outlook.com>
2023-01-23 16:41:08 +0100

    Update web page

---
+++ docs/scripts/add-releases.js
@@ -0,0 +1,31 @@
+function addReleases() {
+    var releases = [
+        { version: "2.23.1.18 (latest)", fileName: "QualiTTVBot_2_23_1_18.zip" },
+    ]
+    
+    releases.forEach(function (release) {
+        var filePath = "releases/" + release.fileName;
+        
+        var xhr = new XMLHttpRequest();
+
+        xhr.open("HEAD", filePath, true);
+
+        xhr.onreadystatechange = function () {
+            if (this.readyState === this.DONE) {
+                var fileSize = parseInt(xhr.getResponseHeader("Content-Length"));
+                var table = document.getElementById("file-table");
+
+                var row = table.insertRow();
+                var versionCell = row.insertCell(0);
+                var fileSizeCell = row.insertCell(1);
+                var downloadCell = row.insertCell(2);
+
+                versionCell.innerHTML = release.version;
+                fileSizeCell.innerHTML = (fileSize / 1024 / 1024).toFixed(2) + " MB";
+                downloadCell.innerHTML = "<a href='" + release.fileName + "' class='btn btn-outline-primary'><i class='bi bi-download'></i> " + release.fileName + "</a>";
+            }
+        };
+
+        xhr.send();
+    });
+}
